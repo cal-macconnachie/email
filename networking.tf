f9f671203c5481a6029bcc0d84b2a634d606e346
cal macconnachie <cal.macconnachie@gmail.com>
2025-12-29 21:16:17 -0800

    feat: domain registration on ses

--- networking.tf
+++ networking.tf
@@ -143,3 +143,21 @@ resource "aws_route53_record" "cloudfront" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_ses_domain_identity" "main" {
+  domain = var.domain_name
+}
+  
+resource "aws_route53_record" "main_amazonses_verification_record" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "_amazonses.${aws_ses_domain_identity.main.domain}"
+  type    = "TXT"
+  ttl     = "600"
+  records = [aws_ses_domain_identity.main.verification_token]
+}
+
+resource "aws_ses_domain_identity_verification" "main_verification" {
+  domain = aws_ses_domain_identity.main.domain
+
+  depends_on = [aws_route53_record.main_amazonses_verification_record]
+}
