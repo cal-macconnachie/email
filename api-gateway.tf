2546b345707773eb2d70361bc18d570a39adffb1
cal macconnachie <cal.macconnachie@gmail.com>
2025-12-31 09:52:21 -0800

    fix: suspect this wont work

--- api-gateway.tf
+++ api-gateway.tf
@@ -103,7 +103,7 @@ resource "aws_apigatewayv2_integration" "get_thread_emails" {
   api_id                 = aws_apigatewayv2_api.main.id
   integration_type       = "AWS_PROXY"
   integration_uri        = aws_lambda_function.functions["get_thread_emails"].invoke_arn
-  integration_method     = "GET"
+  integration_method     = "POST"
   payload_format_version = "2.0"
 }
 
