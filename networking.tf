f6a26fb75dd1af3f1e539ec38e5df4d6e5748208
cal macconnachie <cal.macconnachie@gmail.com>
2025-12-31 09:58:49 -0800

    fix: allow read

--- networking.tf
+++ networking.tf
@@ -43,6 +43,26 @@ resource "aws_s3_bucket_public_access_block" "website" {
   restrict_public_buckets = false
 }
 
+# S3 Bucket Policy for public website access
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.website]
+}
+
 # ACM Certificate
 resource "aws_acm_certificate" "main" {
   domain_name               = var.domain_name
